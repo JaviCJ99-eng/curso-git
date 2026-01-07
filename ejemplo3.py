85b78cf64ba2b9b3fcf94f4f2516e4b2292c9b3f
Javier J <cristian.cj99@gmail.com>
2026-01-07 10:31:58 -0500

    Creacion del 3er archivo

---
+++ ejemplo3.py
@@ -0,0 +1,12 @@
+print("Archivo 3")
+
+mixta = [10, "hola", True, 3.14]
+
+# Acceder al primer elemento (índice 0)
+print(mixta[0]) # Salida: 10
+
+# Acceder al segundo elemento (índice 1)
+print(mixta[1]) # Salida: hola
+
+# Obtener la longitud
+print(len(mixta)) # Salida: 4
