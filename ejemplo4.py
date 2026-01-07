ded6d35d5e0500f3dab995794d3ade8de6ed69cd
Javier J <cristian.cj99@gmail.com>
2026-01-07 11:04:07 -0500

    Se prueba libreria bcrypt en ejemplo 4

--- ejemplo4.py
+++ ejemplo4.py
@@ -5,4 +5,13 @@ salt = bcrypt.gensalt()           # Generate a random salt
 hashed_password = bcrypt.hashpw(password, salt)
 
 # The hashed password (including the salt) can be stored in a database
-print(hashed_password)
+#print(hashed_password)
+
+pass1 = bcrypt.hashpw("cristianCrack".encode(), bcrypt.gensalt())
+pass2 = bcrypt.hashpw("cristianCrack4".encode(), bcrypt.gensalt())
+pass3 = b'$2b$12$V6BAjoezeFWJyOY8uBJHvOWlWravkFJHSJ4l2kXhIwoXdCRMlLnL6'
+print(pass1)
+print(pass2)
+print(pass3)
+
+print(bcrypt.checkpw("cristianCrack".encode(), pass3))
