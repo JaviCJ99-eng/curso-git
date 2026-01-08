87eef79c9d6e07b366a13a4849ca95dcf34ec008
Javier J <cristian.cj99@gmail.com>
2026-01-08 11:00:07 -0500

    ejemplo 1 formacion de matriz 5x5

--- ejemplo1.py
+++ ejemplo1.py
@@ -3,3 +3,13 @@ print("Script 1 ejecutado")
 print("Nueva info en Script 1")
 
 print(2+3)
+
+print("Realizo cambios en el ejemplo 1")
+
+a = [] 
+b =  [ [] for i in range(5) ]
+c =  [[ [] for i in range(5) ] for j in range(5)]
+c[4][0] = "JA"
+c[3][3:] = ["CRIStia"] * 2 
+
+print(c)
